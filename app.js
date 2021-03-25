4e9dc6f85214aa53a2abc163fbc549fe9717986b
L3p1989 <phogan110@live.com>
2021-03-24 21:55:36 -0700

    changed class name of div.dice to die

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //assign new class Die
 class Die {
   constructor() {
-    this.newDie = $(`<div class="dice"></div>`);
+    this.newDie = $(`<div class="die"></div>`);
     this.value = Math.floor(Math.random() * 6) + 1;
-    this.newDie.text(this.value);
+    this.newDie.text(this.value).appendTo(`#die-div`);
   }
   roll() {
     let sum = Math.floor(Math.random() * 6) + 1;
