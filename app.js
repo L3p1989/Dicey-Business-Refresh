af599891e08645ff5a4cb89dda85f599fe6d8b9a
L3p1989 <phogan110@live.com>
2021-03-24 23:15:57 -0700

    assigned static method rollAll to Die. assigned click handler to #roll-btn that calls rollAll

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 //assign new class Die
 class Die {
   static dice = [];
+  static rollAll() {
+    Die.dice.forEach((die) => {
+      die.roll();
+    });
+  }
   constructor() {
     this.newDie = $(`<div class="die"></div>`);
     this.value = this.roll();
@@ -9,9 +14,8 @@ class Die {
       this.newDie.remove();
       Die.dice.splice(this.newDie, 1);
     });
-    Die.dice.push(this.newDie);
+    Die.dice.push(this);
     this.newDie.appendTo(`#die-div`);
-    console.log(Die.dice);
   }
   roll() {
     this.value = Math.floor(Math.random() * 6) + 1;
@@ -20,3 +24,5 @@ class Die {
 }
 //give die-btn click listener
 $(`#die-btn`).click(() => new Die());
+//give roll-btn click handler
+$(`#roll-btn`).click(() => Die.rollAll());
