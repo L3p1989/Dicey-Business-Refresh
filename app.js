9eb0a8e8246f8dc2b98f43179fc71e5857a99a1c
L3p1989 <phogan110@live.com>
2021-03-24 22:44:59 -0700

    assigned static property dice array, when new die is created adds to dice array, when die is removed splices from dice array

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //assign new class Die
 class Die {
+  static dice = [];
   constructor() {
     this.newDie = $(`<div class="die"></div>`);
     this.value = this.roll();
     this.newDie.click(() => this.roll());
-    this.newDie.dblclick(() => this.newDie.remove());
+    this.newDie.dblclick(() => {
+      this.newDie.remove();
+      Die.dice.splice(this.newDie, 1);
+    });
+    Die.dice.push(this.newDie);
     this.newDie.appendTo(`#die-div`);
+    console.log(Die.dice);
   }
   roll() {
     this.value = Math.floor(Math.random() * 6) + 1;
